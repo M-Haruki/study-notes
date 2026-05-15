c0e8fc936a78a724d060345956f77a37c1dc1855
M-Haruki <105488840+M-Haruki@users.noreply.github.com>
2026-05-15 04:47:41 +0000

    サーバーアドレスを環境変数から取得するように設定を更新

--- backend/main.go
+++ backend/main.go
@@ -21,6 +21,7 @@ type Config struct {
 	ContextUserIDKey string
 	IsProduction     bool
 	DatabaseURL      string
+	ServerAddress    string
 }
 
 var AppConfig Config
@@ -38,8 +39,9 @@ func main() {
 		JwtExpires:       24 * time.Hour,
 		JwtSecret:        os.Getenv("JWT_SECRET"),
 		DatabaseURL:      os.Getenv("DATABASE_URL"),
+		ServerAddress:    os.Getenv("SERVER_ADDRESS"),
 	}
-	if AppConfig.JwtSecret == "" || AppConfig.DatabaseURL == "" {
+	if AppConfig.JwtSecret == "" || AppConfig.DatabaseURL == "" || AppConfig.ServerAddress == "" {
 		log.Fatal("Invalid Environment")
 	}
 	switch os.Getenv("ENV") {
@@ -94,7 +96,7 @@ func main() {
 	})
 
 	// run server
-	if err := e.Start(":1323"); err != nil {
+	if err := e.Start(AppConfig.ServerAddress); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
 	}
 }
